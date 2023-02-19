cde12c0be721427a66c36050f6886e2d04def065
VitorTz <vitor.ftz@outlook.com>
2023-02-19 02:54:37 -0300

    Update

---
+++ src/level.py
@@ -0,0 +1,18 @@
+import pygame 
+import settings
+
+
+class Level:
+
+	def __init__(self):
+
+		# get the display surface
+		self.display_surface = pygame.display.get_surface()
+
+		# sprite groups
+		self.all_sprites = pygame.sprite.Group()
+
+	def run(self, dt: float):
+		self.display_surface.fill(settings.SCREN_BG_COLOR)
+		self.all_sprites.draw(self.display_surface)
+		self.all_sprites.update()
